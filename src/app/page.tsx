041089713396e820ba016253424bf22baa7df7de
aminevg <38422328+aminevg@users.noreply.github.com>
2024-04-18 23:05:37 +0900

    Force dynamic page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const posts = await db.query.posts.findMany();
 
