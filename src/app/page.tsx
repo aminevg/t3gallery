de8f8bb934644ba4820457fbaad0216b7b1dbefd
aminevg <38422328+aminevg@users.noreply.github.com>
2024-04-18 22:51:16 +0900

    Setup DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,24 @@
 import Image from "next/image";
+import { db } from "~/server/db";
+
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  return (
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {IMAGE_URLS.map((image) => (
+          <div key={image.id} className="h-32 w-40">
+            <Image src={image.url} width="160" height="128" alt="image" />
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
 
 const IMAGE_URLS = [
   {
@@ -17,20 +37,55 @@ const IMAGE_URLS = [
     id: 4,
     url: "https://hkhfdilwebjcbckqizyv.supabase.co/storage/v1/object/public/t3gallery/image4.jpg",
   },
-] as const;
 
-export default function HomePage() {
-  return (
-    <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...IMAGE_URLS, ...IMAGE_URLS, ...IMAGE_URLS, ...IMAGE_URLS].map(
-          (image) => (
-            <div key={image.id} className="h-32 w-40">
-              <Image src={image.url} width="160" height="128" alt="image" />
-            </div>
-          ),
-        )}
-      </div>
-    </main>
-  );
-}
+  {
+    id: 5,
+    url: "https://hkhfdilwebjcbckqizyv.supabase.co/storage/v1/object/public/t3gallery/image1.jpg",
+  },
+  {
+    id: 6,
+    url: "https://hkhfdilwebjcbckqizyv.supabase.co/storage/v1/object/public/t3gallery/image2.jpg",
+  },
+  {
+    id: 7,
+    url: "https://hkhfdilwebjcbckqizyv.supabase.co/storage/v1/object/public/t3gallery/image3.jpg",
+  },
+  {
+    id: 8,
+    url: "https://hkhfdilwebjcbckqizyv.supabase.co/storage/v1/object/public/t3gallery/image4.jpg",
+  },
+
+  {
+    id: 9,
+    url: "https://hkhfdilwebjcbckqizyv.supabase.co/storage/v1/object/public/t3gallery/image1.jpg",
+  },
+  {
+    id: 10,
+    url: "https://hkhfdilwebjcbckqizyv.supabase.co/storage/v1/object/public/t3gallery/image2.jpg",
+  },
+  {
+    id: 11,
+    url: "https://hkhfdilwebjcbckqizyv.supabase.co/storage/v1/object/public/t3gallery/image3.jpg",
+  },
+  {
+    id: 12,
+    url: "https://hkhfdilwebjcbckqizyv.supabase.co/storage/v1/object/public/t3gallery/image4.jpg",
+  },
+
+  {
+    id: 13,
+    url: "https://hkhfdilwebjcbckqizyv.supabase.co/storage/v1/object/public/t3gallery/image1.jpg",
+  },
+  {
+    id: 14,
+    url: "https://hkhfdilwebjcbckqizyv.supabase.co/storage/v1/object/public/t3gallery/image2.jpg",
+  },
+  {
+    id: 15,
+    url: "https://hkhfdilwebjcbckqizyv.supabase.co/storage/v1/object/public/t3gallery/image3.jpg",
+  },
+  {
+    id: 16,
+    url: "https://hkhfdilwebjcbckqizyv.supabase.co/storage/v1/object/public/t3gallery/image4.jpg",
+  },
+] as const;
